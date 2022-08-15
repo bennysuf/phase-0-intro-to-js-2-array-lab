7ae78a2bd3c67f621ff5458539ec25142c2e3ab8
Benny Sufrin <bennysuf@gmail.com>
2022-08-15 11:13:25 -0400

    commit tree a1b922071a499c89cd73687b15100766783903b6

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+destructivelyAppendCat = (name) => {
+    return cats.push("Ralph")
+}
+
+function destructivelyPrependCat(name) {
+ return cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift()
+}
+
+function appendCat(name) {
+   const myCats = [...cats, "Broom"];
+   return myCats
+}
+
+function prependCat(name) {
+    const myCats = ["Arnold", ...cats];
+    return myCats
+}
+
+function removeLastCat() {
+    return cats.slice(0, 2)
+}
+
+function removeFirstCat() {
+    return cats.slice(1,3)
+
+}
